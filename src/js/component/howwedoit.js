f5dc6717cc7ce156fa965d89de271a2036a8523b
lucciii33 <angelomaiele@gmail.com>
2022-05-09 21:25:37 +0000

    carousel

--- src/js/component/howwedoit.js
+++ src/js/component/howwedoit.js
@@ -21,21 +21,21 @@ export const HowWeDoIt = () => {
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={image3} className="d-block w-100" alt="..."/>
+      <img src={image4} className="caroimage d-block" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src={image4} className="d-block w-100" alt="..."/>
+      <img src={image4} className=" caroimage d-block w-100" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src={image4} className=" caroimage d-block w-100" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
